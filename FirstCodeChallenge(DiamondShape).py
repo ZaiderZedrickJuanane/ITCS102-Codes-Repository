d75d1390d6af4d1dd2834e9f225bc7eb3bdac0ec
ZaiderZedrickJuanane <juananezaiderzedrick@gmail.com>
2026-08-13 20:08:53 +0800

    August 13 2026

---
+++ FirstCodeChallenge(DiamondShape).py
@@ -0,0 +1,10 @@
+Text_Output = "\t\t\t\t\t\t\t\t\t\t\t * "
+Text_Output2 = "\t\t\t\t\t\t\t\t\t\t * \t\t * "
+Text_Output3 = "\t\t\t\t\t\t\t\t\t *  \t\t\t *"
+Text_Output4 = "\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *"
+Text_Output5 = "\t\t\t\t\t\t\t * \t\t   Hi My name is, \t\t *"
+Text_Output4v1 = "\t\t\t\t\t\t\t\t * \t\t  Juanane \t\t *"
+
+Final_Output = "\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t *  \t\t\t *\n\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *\n\t\t\t\t\t\t\t * \t\t   Hi My name is, \t\t *\n\t\t\t\t\t\t\t\t * \t\t  Juanane \t\t *\n\t\t\t\t\t\t\t\t\t *  \t\t\t *\n\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t\t\t * "
+
+print(Final_Output)
