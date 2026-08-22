3b3c0a1fa4f72f682eaa86c63302e175cdee9ad0
ZaiderZedrickJuanane <juananezaiderzedrick@gmail.com>
2026-08-22 08:48:53 +0800

    Update FirstCodeChallenge(DiamondShape).py

--- FirstCodeChallenge(DiamondShape).py
+++ FirstCodeChallenge(DiamondShape).py
@@ -1,10 +1,29 @@
-Text_Output = "\t\t\t\t\t\t\t\t\t\t\t * "
-Text_Output2 = "\t\t\t\t\t\t\t\t\t\t * \t\t * "
-Text_Output3 = "\t\t\t\t\t\t\t\t\t *  \t\t\t *"
-Text_Output4 = "\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *"
-Text_Output5 = "\t\t\t\t\t\t\t * \t\t   Hi My name is, \t\t *"
-Text_Output4v1 = "\t\t\t\t\t\t\t\t * \t\t  Juanane \t\t *"
+Text_Output = "\t\t\t\t\t\t\t\t\t\t\t\t\t * "
+Text_Output2 = "\t\t\t\t\t\t\t\t\t\t\t\t * \t\t * "
+Text_Output3 = "\t\t\t\t\t\t\t\t\t\t\t *  \t\t\t\t *"
+Text_Output4 = "\t\t\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *"
+Text_Output5 = "\t\t\t\t\t\t\t\t\t * \t\t\t   Hi My name is, \t\t\t *"
+Text_Output6 = "\t\t\t\t\t\t\t\t * \t\t\t     Zaider Zedrick S. Juanane \t\t\t\t *"
+Text_Output5v1 = "\t\t\t\t\t\t\t\t\t * \t\t\t\t\t\t\t\t *"
 
-Final_Output = "\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t *  \t\t\t *\n\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *\n\t\t\t\t\t\t\t * \t\t   Hi My name is, \t\t *\n\t\t\t\t\t\t\t\t * \t\t  Juanane \t\t *\n\t\t\t\t\t\t\t\t\t *  \t\t\t *\n\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t\t\t * "
+
+#test
+#print(Text_Output)
+#print(Text_Output2)
+#print(Text_Output3)
+#print(Text_Output4)
+#print(Text_Output5)
+#print(Text_Output6)
+#print(Text_Output5v1)
+#print(Text_Output4)
+#print(Text_Output3)
+#print(Text_Output2)
+#print(Text_Output)
+
+
+#final
+Final_Output = "\t\t\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t\t\t *  \t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *\n\t\t\t\t\t\t\t\t\t * \t\t\t   Hi My name is, \t\t\t *\n\t\t\t\t\t\t\t\t * \t\t\t     Zaider Zedrick S. Juanane \t\t\t\t *\n\t\t\t\t\t\t\t\t\t * \t\t\t\t\t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t * \t\t\t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t\t *  \t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t\t\t * \t\t * \n\t\t\t\t\t\t\t\t\t\t\t\t\t * "
+
+
 
 print(Final_Output)
