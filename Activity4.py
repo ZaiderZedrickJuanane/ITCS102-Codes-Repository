e830fdd45234cd4b9735ee8d3024b4e44be8ead3
ZaiderZedrickJuanane <316561761+ZaiderZedrickJuanane@users.noreply.github.com>
2026-09-03 11:35:23 +0800

    Activities

---
+++ Activity4.py
@@ -0,0 +1,12 @@
+# Escape sequence Is used to modify inline strings
+#\n
+#\t
+#\r
+#\\
+#\""
+#\b
+
+motto2 = "The Quick Brown Fox Jumps over the Lazy Dog\r. A negative \n mind would never give you a positive life"
+
+
+print(motto2)
